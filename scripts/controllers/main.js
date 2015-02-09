ba1bc054e4acb3ba4ae4dc7ae3b8dd941da95bcf
yanglingang <lingang.scm@gmail.com>
2015-02-09 20:52:49 +0800

    2-9 1

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -8,10 +8,31 @@
  * Controller of the wwwApp
  */
 angular.module('wwwApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
+
+
+jQuery(function($) {
+    $(document).ready(function() {
+        //enabling stickUp on the '.navbar-wrapper' class
+        $('#stickUpBar').stickUp({
+            //enabling marginTop with the 'auto' setting parts: {
+            parts: {
+                0: 'section1',
+                1: 'section2',
+                2: 'section3',
+                3: 'section4',
+                4: 'section5',
+                5: 'section6'
+            },
+            itemClass: 'menuItem',
+            itemHover: 'menuItem-Active',
+            marginTop: 'auto'
+        });
+    });
+});
